cca2c71e268d26c911ee46affa1112cc75626ff4
“Emilio” <emilio.jeldes@gmail.com>
2018-06-07 23:48:55 -0400

    cors

--- src/main/java/cl/ewebs/pblog/controller/TaskController.java
+++ src/main/java/cl/ewebs/pblog/controller/TaskController.java
@@ -9,43 +9,49 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/tasks")
+@CrossOrigin
 public class TaskController {
 
-    private final TaskService taskService;
+	private final TaskService taskService;
 
-    @Autowired
-    public TaskController(TaskService taskService) {
-        this.taskService = taskService;
-    }
+	@Autowired
+	public TaskController(TaskService taskService) {
+		this.taskService = taskService;
+	}
 
-    @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    public TaskListDTO getAllTasks() {
-        return new TaskListDTO(taskService.getAllTasks());
-    }
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	@CrossOrigin
+	public TaskListDTO getAllTasks() {
+		return new TaskListDTO(taskService.getAllTasks());
+	}
 
-    @GetMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public TaskDTO getTaskById(@PathVariable Long id) {
-        return taskService.findTaskById(id);
-    }
+	@GetMapping("/{id}")
+	@CrossOrigin
+	@ResponseStatus(HttpStatus.OK)
+	public TaskDTO getTaskById(@PathVariable Long id) {
+		return taskService.findTaskById(id);
+	}
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public TaskDTO createTask(@RequestBody TaskDTO taskDTO) {
-        return taskService.saveTask(taskDTO);
-    }
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	@CrossOrigin
+	public TaskDTO createTask(@RequestBody TaskDTO taskDTO) {
+		return taskService.saveTask(taskDTO);
+	}
 
-    @PutMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public TaskDTO updateTask(@PathVariable Long id, @RequestBody TaskDTO taskDTO) {
-        return taskService.updateTask(id, taskDTO);
-    }
+	@PutMapping("/{id}")
+	@CrossOrigin
+	@ResponseStatus(HttpStatus.OK)
+	public TaskDTO updateTask(@PathVariable Long id, @RequestBody TaskDTO taskDTO) {
+		return taskService.updateTask(id, taskDTO);
+	}
 
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public void deleteTask(@PathVariable Long id) {
-        taskService.deleteTask(id);
-    }
+	@DeleteMapping("/{id}")
+	@CrossOrigin
+	@ResponseStatus(HttpStatus.OK)
+	public void deleteTask(@PathVariable Long id) {
+		taskService.deleteTask(id);
+	}
 
 }

0cd1dc79a7100be5d8727f86041f1d5d09701d90
“Emilio” <emilio.jeldes@gmail.com>
2018-06-09 03:19:47 -0400

    imports

--- src/test/java/cl/ewebs/pblog/controller/RestResponseEntityExceptionHandlerTest.java
+++ src/test/java/cl/ewebs/pblog/controller/RestResponseEntityExceptionHandlerTest.java
@@ -1,22 +1,17 @@
 package cl.ewebs.pblog.controller;
 
-import cl.ewebs.pblog.api.model.ApiError;
 import cl.ewebs.pblog.exceptions.ResourceNotFoundException;
 import cl.ewebs.pblog.services.TaskService;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
